6eb9bb009b416c5f686d42826d28fee8536c2d42
Rajesh Thummalapally <rthummalapally@intellicentrics.com>
2016-04-21 08:09:58 -0500

    Updating README.md; Changing random tag names in tests to meaningful ones

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # EntityTagger
 
 Ruby version: ruby 2.2.1p85
+
+## Setting up locally
+After cloning and doing a bundle install, install PostgreSQL with homebrew
+```bash
+brew install postgresql
+```
+
+You'll need to create the mvp user with password 'secret' like so
+```bash
+psql -u root -p
+psql> CREATE USER 'mvp'@'localhost' IDENTIFIED BY 'secret';
+psql> GRANT ALL PRIVILEGES ON * . * TO 'mvp'@'localhost';
+psql> quit
+```
+
+Before testing the apis, run
+```bash
+rake db:create db:migrate
+```
+
+You can run specs using
+```bash
+rake spec
+```
+
+Notes:
+Any model specified by the constraints in the routes can be tagged. As such, Taggings are polymorphic.
+I chose to use a service object for handling all the business logic to create entities and tags.
+That way, the controller doesn't know anything about it and can be easily maintainable.
+I did not use FactoryGirl or fixtures since the existing no. of attributes on the models are small.
+
+Requirements.md asks 'GET /stats/:entity_type/:entity_id' to 'Retrives statistics about a specific tagged entity'. 
+I could be missing something, but Other than tags, are there any other stats?
+Since the stats of a specific entity can be accessed using 'GET /tags/:entity_type/:entity_id', 
+I changed the url to 'stats/tags/:tag_id' to return stats about a tag.
+
--- spec/services/tag_service_spec.rb
+++ spec/services/tag_service_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe TagService do
   {
     :entity_type=>"Article", 
     :entity_name=>"Hillary", 
-    :tags=>["old", "democrat", "cheddadi", "pora"]
+    :tags=>["lady", "democrat", "secretary", "experienced"]
   } 
   )}
 
  let (:existing_entity_tag_service) { TagService.new(
   {
     :entity_type=>"Article", 
-    :entity_name=>"Hillary", 
+    :entity_name=>"Sanders", 
     :entity_id => '1',
-    :tags=>["old", "democrat", "cheddadi", "pora"]
+    :tags=>["democrat", "senator", "experienced"]
   } 
   )}
 
@@ -23,22 +23,22 @@ RSpec.describe TagService do
    existing_entity_tag_service.create_tags
    TagService.new({
     :entity_type=>"Article", 
-    :entity_name=>"Hillary", 
+    :entity_name=>"Sanders", 
     :entity_id => '1',
-    :tags=>["old", "democrat", "cheddadi", "pora"]
+    :tags=>["democrat", "senator", "experienced"]
    }).create_tags
-  expect(Tag.where(name: ["old", "democrat", "cheddadi", "pora"]).count).to eql(4)
+  expect(Tag.where(name: ["democrat", "senator", "experienced"]).count).to eql(3)
  end
 
  it "should over-write tags for a entity" do
    product = Product.create(name: 'Book')
-   ['old', 'politics', 'classic', 'fiction'].each {|name| Tag.create(name: name)}
+   ['politics', 'classic', 'fiction'].each {|name| Tag.create(name: name)}
    TagService.new({
     :entity_type=>"Product", 
     :entity_id => product.id,
-    :tags=>["old", "democrat", "cheddadi", "pora"]
+    :tags=>["democrat", "secretary", "experienced", "fiction"]
    }).create_tags
-   expect(Product.find_by(name: 'Book').tags.pluck(:name)).to eql(["old", "democrat", "cheddadi", "pora"])
+   expect(Product.find_by(name: 'Book').tags.pluck(:name)).to eql(["democrat", "secretary", "experienced", "fiction"])
  end
  
  it "should create entity for new entities" do
